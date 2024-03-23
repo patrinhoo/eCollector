de32150599a37ada48c55932e3d19b954599c05f
patrinhoo <patrinhorybacki@gmail.com>
2024-03-23 17:57:23 +0100

    EC-002: feat: filtering, searching, fix: sorting

--- api/views.py
+++ api/views.py
@@ -3,8 +3,8 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-
-from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiParameter
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from . import models
 from . import serializers
@@ -29,33 +29,19 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
         return self.request.user
 
 
-@extend_schema_view(
-    list=extend_schema(
-        parameters=[
-            OpenApiParameter(name='sortField', description='Type sort field', type=str),
-            OpenApiParameter(name='sortOrder', description='Type sort order (ascend/descend)', type=str),
-        ]
-    )
-)
 class CardViewSet(viewsets.ModelViewSet):
     queryset = models.Card.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['catalog_number', 'name']
+    search_fields = ['catalog_number', 'name']
+    ordering_fields = ['id', 'catalog_number', 'name', 'status']
+    ordering = ['-id']
+
     def get_queryset(self):
         queryset = super().get_queryset()
-
-        field = self.request.query_params.get('sortField')
-        order = self.request.query_params.get('sortOrder')
-
-        if field is not None:
-            sort_field = field
-            if order == 'descend':
-                sort_field = '-' + sort_field
-
-            queryset = queryset.order_by(sort_field)
-        else:
-            queryset = queryset.order_by('-id')
         
         return queryset.filter(user=self.request.user).distinct()
 
