3a9246c44af474d8573baaef5eaaced34b47848e
patrinhoo <81069467+patrinhoo@users.noreply.github.com>
2024-04-17 01:23:20 +0200

    EC-017: Dockerize project

--- app/eCollector/urls.py
+++ app/eCollector/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
         name='api-docs',
     ),
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
